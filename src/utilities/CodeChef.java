5731936332ed2d0870ff0e7ecb53efebc1472b6b
Ismael Flores <iflores@circontrol.com>
2023-10-24 09:00:39 +0200

    Added CodeChef problem solution EQUALCOIN and avoid unused variable N in ZEROSTRING problem

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -2005,7 +2005,7 @@ public class CodeChef {
         Scanner scn = new Scanner(System.in);
         long T = scn.nextLong();
         while (T-- > 0) {
-            long N = scn.nextLong();
+            scn.nextLong();
             String S = scn.next();
             long ones = 0;
             long zeros = 0;
@@ -2076,6 +2076,46 @@ public class CodeChef {
             long N = scn.nextInt();
             boolean posible = (N % 2 == 0 || N == 1);
             System.out.println(posible ? "YES" : "NO");
+        }
+        scn.close();
+    }
+
+    /*
+     * Problem:
+     * Read problem statements in Bengali, Russian, Mandarin and Vietnamese as well.
+     * Chef has X coins worth 1 rupee each and Y coins worth 2 rupees each. 
+     * He wants to distribute all of these X+Y coins to his two sons so that the total value of coins received by each of them is the same. 
+     * Find out whether Chef will be able to do so.
+     * 
+     * Input Format:
+     * The first line of input contains a single integer T, denoting the number of testcases. 
+     * The description of T test cases follows.
+     * Each test case consists of a single line of input containing two space-separated integers X and Y.
+     * 
+     * Output Format:
+     * For each test case, print "YES" (without quotes) if Chef can distribute all the coins equally and "NO" otherwise. 
+     * You may print each character of the string in uppercase or lowercase 
+     * (for example, the strings "yEs", "yes", "Yes" and "YES" will all be treated as identical).
+     * 
+     * Constraints:
+     * 1 <= T <= 10^3
+     * 0 <= X,Y <= 10^8
+     * X + Y > 0
+     */
+    public static void solveCodeChefEQUALCOIN() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long X = scn.nextLong();
+            long Y = scn.nextLong();
+            if (X % 2 == 1)
+                System.out.println("NO");
+            else if (X >= 2 && X % 2 == 0)
+                System.out.println("YES");
+            else if (Y % 2 == 0)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
         scn.close();
     }
