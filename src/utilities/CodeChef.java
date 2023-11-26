dc93c032bdd1152d049b349ae21addc794f71802
Ismael Flores <iflores@circontrol.com>
2023-11-26 21:01:39 +0100

    Added CodeChef problem solutions CWC23QUALIF, VIDEOWORTH and BESTOFTENNIS

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -2749,6 +2749,110 @@ public class CodeChef {
     }
 
     /*
+    * Problem:
+    * The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. 
+    * Any team that scores 12 or more points in the group stage matches qualifies for the next stage.
+    * You know the score that a particular team has scored in the group stage matches. 
+    * Determine if the team has qualified for the next stage or not.
+    *
+    * Input Format:
+    * The first and only line of input consists of an integer X 
+    * denoting the total points scored by the given team in the group stage matches.
+    *
+    * Output Format:
+    * Output Yes, if the team has qualified for the next stage, and No otherwise.
+    * You may print each character of the string in uppercase or lowercase 
+    * (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+    *
+    * Constraints:
+    * 1 <= X <= 20
+    */
+    public static void solveCodeChefCWC23QUALIF() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong();
+        System.out.println(X >= 12 ? "Yes" : "No");
+        scn.close();
+    }
+
+    /*
+    * Problem:
+    * We know that "A picture is worth a thousand words". So let's calculate the worth of a video using this!
+    * Suppose a video has 24 frames (or pictures) per second, and has a duration of S seconds. 
+    * We know that each frame is worth 1000 words. So, how many words is this video worth in total?
+    *
+    * Input Format:
+    * The first line of input will contain a single integer T, denoting the number of test cases.
+    * Each test case consists of a single line with a single integer S, the number of seconds in the video.
+    *
+    * Output Format:
+    * For each test case, output on a new line the number of words that the video is worth.
+    *
+    * Constraints:
+    * 1 <= T <= 100
+    * 1 <= S <= 100
+    */
+    public static void solveCodeChefVIDEOWORTH() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long S = scn.nextLong();
+            System.out.println(S * 24 * 1000);
+        }
+        scn.close();
+    }
+
+    /*
+    * Problem:
+    * Sonu and Titu are playing a tennis match. 
+    * They are playing a "best of N sets" match (N is always odd). 
+    * That means that they will play at most N sets, 
+    * and the person who has won most sets wins the match. 
+    * But they are smart, and if they notice at any point that one of them has no chance of winning the match, 
+    * they will stop the match right then, without playing out all the N sets needlessly.
+    * 
+    * For example, suppose N = 11. And at some point, Sonu has won 5 sets, and Titu has won 4 sets. 
+    * Sonu is leading now, but it is possible that Titu wins the two remaining sets and wins the whole match. 
+    * So they will not stop right now.
+    *
+    * But suppose Sonu wins the next set as well, and so now the score is (6, 4). 
+    * Now, even though there is one set remaining, there is no chance of Titu winning the whole match, 
+    * since at best, even if he wins the next set, the score will become (6,5), 
+    * and Sonu still wins. So, they will stop at a score of (6, 4) and declare Sonu as the winner.
+    *
+    * As another example, if N = 15, and the score is currently (8, 2), 
+    * then they'll stop right now, since there is no way for Titu to win the match. 
+    * Even if Titu wins all the 5 remaining sets, Titu cannot win the match.
+    * You are watching the match, and you see that the match has ended with a score of (X, Y). 
+    * That is, Sonu has won X sets, and Titu has won Y sets. But you don't know what N is. 
+    * Can you find out N from just knowing the final score?
+    *
+    * Input Format:
+    * The first line of input will contain a single integer T, denoting the number of test cases.
+    * Each test case consists of a single line of input containing two space-separated integers X and Y 
+    * where X is the number of sets won by Sonu and Y is the number of sets won by Titu.
+    *
+    * Output Format:
+    * For each test case, output on a new line the value of N.
+    * Note that you should only output an odd integer.
+    *
+    * Constraints:
+    * 1 <= T <= 10000
+    * 1 <= X <= 100
+    * 1 <= Y <= 100
+    * X != Y, since they would never stop when they have won the same number of sets.
+    */
+    public static void solveCodeChefBESTOFTENNIS() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long X = scn.nextLong();
+            long Y = scn.nextLong();
+            System.out.println(Math.max(X, Y) * 2 - 1);
+        }
+        scn.close();
+    }
+    
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
