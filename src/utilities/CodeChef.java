21725322f6236b5c7587e5ae38654c631c1810e6
Ismael Flores <iflores@circontrol.com>
2024-03-08 11:44:18 +0100

    Add solution for Chef and Table Tennis problem (TTENIS)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -4452,6 +4452,46 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Chef and Table Tennis
+     * 
+     * Chef likes to play table tennis. He found some statistics of matches which described who won the points in order.
+     * A game shall be won by the player first scoring 11 points except in the case when both players have 10 points each, 
+     * then the game shall be won by the first player subsequently gaining a lead of 2 points.
+     * Could you please help the Chef find out who the winner was from the given statistics? 
+     * (It is guaranteed that statistics represent always a valid, finished match.)
+     * 
+     * Input:
+     * The first line of the input contains an integer T, denoting the number of test cases. 
+     * The description of T test cases follows. Each test case consist a binary string S, which describes a match. 
+     * '0' means Chef lose a point, whereas '1' means he won the point.
+     * 
+     * Output:
+     * For each test case, output on a separate line a string describing who won the match. 
+     * If Chef won then print "WIN" (without quotes), otherwise print "LOSE" (without quotes).
+     * 
+     * Constraints:
+     * 1 <= T <= 1000
+     * 1 <= length(S) <= 100
+     */
+    public static void solveCodeChefTTENIS() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            String S = scn.next();
+            long ones = 0;
+            long zeros = 0;
+            for (char c : S.toCharArray()) {
+                if (c == '1')
+                    ones++;
+                else
+                    zeros++;
+            }
+            System.out.println(ones > zeros ? "WIN" : "LOSE");
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
