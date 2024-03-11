b210b05b01d3c21d7c3e0f2b6127fe07bf9417a0
Ismael Flores <iflores@circontrol.com>
2024-03-11 09:42:08 +0100

    Add solution for N Queens Puzzle EUREKA

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -4492,6 +4492,45 @@ public class CodeChef {
     }
 
     /*
+     * Problem: N Queens Puzzle Solved!
+     * 
+     * Chef, being a Chess fan, was thrilled after he read the following news:
+     * Michael Simkin, a postdoctoral fellow at Harvard University’s Center of Mathematical Sciences and Applications 
+     * proved that for a large value of N, there are approximately (0.143 * N) ^ N configurations 
+     * in which N queens can be placed on a N x N chessboard so that none attack each other.
+     * Although the formula is valid for large N, Chef is interested in finding the value of function f(N) = (0.143 * N) ^ N
+     * for a given small value of N. Since Chef is busy understanding the proof of the formula, please help him calculate this value.
+     * Print the answer rounded to the nearest integer. That is, if the actual value of f(N) is x,
+     * -> Print ⌊x⌋ if x − ⌊x⌋ < 0.5
+     * -> Otherwise, print ⌊x⌋ + 1
+     * ⌊x⌋ denotes the floor of x.
+     * 
+     * Input Format:
+     * The first line of input contains a single integer T, denoting the number of test cases. 
+     * The description of T test cases follows.
+     * Each test case consists of a single line of input containing one integer N.
+     * 
+     * Output Format:
+     * For each test case, output in a single line the value of f(N) rounded to the nearest integer.
+     * 
+     * Constraints:
+     * 1 <= T <= 12
+     * 4 <= N <= 15
+     */
+    public static void solveCodeChefEUREKA() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            double N = (double)scn.nextLong();
+            double base = 0.143 * N;
+            double dresult = Math.pow(base, N);
+            long lresult = Math.round(dresult);
+            System.out.println(lresult);
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
