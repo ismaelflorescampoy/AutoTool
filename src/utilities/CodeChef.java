7c57dae9c2bbbcf13cffcf706b7f7b98fae4f51c
Ismael Flores <iflores@circontrol.com>
2024-03-13 12:11:46 +0100

    Add solution for Chef and Game with Sequence problem L56GAME

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -4669,6 +4669,51 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Chef and Game with Sequence
+     * 
+     * Read problems statements in Mandarin chinese, Russian and Vietnamese as well.
+     * Chef has a sequence A containing N integers A1, A2, ..., AN.
+     * Chef is playing a game with this sequence. 
+     * In this game, he may perform the following operation any number of times (including zero): 
+     * Choose an arbitrary pair of elements from the sequence such that their sum is even, 
+     * delete these two elements from the sequence and insert their sum into the sequence instead.
+     * The goal of the game is to minimise the number of elements in the sequence.
+     * Help Chef to play the game and find the minimum possible size of the sequence at the end of the game!
+     * 
+     * Input:
+     * The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+     * The first line of each test case contains a single integer N.
+     * The second line contains N space-separated integers A1, A2, ..., AN.
+     * 
+     * Output:
+     * For each test case, print a single line containing one integer - the minimum possible final size of the sequence.
+     * 
+     * Constraints:
+     * 1 <= T <= 100
+     * 1 <= N <= 100
+     * 1 <= Ai <= 1000 for each valid i
+     */
+    public static void solveCodeChefL56GAME() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long N = scn.nextLong();
+            long odd = 0;
+            long even = 0;
+            for (long i = 0; i < N; i++) {
+                long Ai = scn.nextLong();
+                if (Ai % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+            even = even + odd / 2;
+            System.out.println((odd % 2) + (even > 0 ? 1 : 0));
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
