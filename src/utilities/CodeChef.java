97c233c5ec97de13a5ff71fa81035132454b8b94
Ismael Flores <iflores@circontrol.com>
2024-03-21 16:30:55 +0100

    Add solveCodeChefPETSTORE method to CodeChef.java with solution for problem "Pet Store" (PETSTORE)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -5001,7 +5001,54 @@ public class CodeChef {
         while (T-- > 0) {
             long N = scn.nextLong();
             System.out.println((N / 2) * 13 + (N % 2) * 6);
+        }
+        scn.close();
+    }
 
+    /*
+     * Problem: Pet Store
+     * 
+     * Alice and Bob went to a pet store. There are N animals in the store where the ith animal is of type Ai.
+     * Alice decides to buy some of these N animals. 
+     * Bob decides that he will buy all the animals left in the store after Alice has made the purchase.
+     * Find out whether it is possible that Alice and Bob end up with exactly same multiset of animals.
+     * 
+     * Input Format:
+     * The first line of input will contain a single integer T, denoting the number of test cases.
+     * Each test case consists of multiple lines of input.
+     * The first line of each test case contains an integer N - the number of animals in the store.
+     * The next line contains NN space separated integers, denoting the type of each animal.
+     * 
+     * Output Format:
+     * For each test case, output on a new line, YES, 
+     * if it is possible that Alice and Bob end up with exactly same multiset of animals and NO otherwise.
+     * You may print each character in uppercase or lowercase. 
+     * For example, the strings YES, yes, Yes, and yES are considered identical.
+     * 
+     * Constraints:
+     * 1 <= T <= 1000
+     * 1 <= N <= 10^5
+     * 1 <= Ai <= 100
+     * The sum of N over all test cases won't exceed 2 * 10^5.
+     */
+    public static void solveCodeChefPETSTORE() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long N = scn.nextLong();
+            HashMap<Long, Long> animals = new HashMap<>();
+            while (N-- > 0) {
+                long Ai = scn.nextLong();
+                animals.put(Ai, animals.getOrDefault(Ai, 0L) + 1);
+            }
+            boolean odd = false;
+            for (long number : animals.values()) {
+                if (number % 2 == 1) {
+                    odd = true;
+                    break;
+                }
+            }
+            System.out.println(odd ? "NO" : "YES");
         }
         scn.close();
     }
